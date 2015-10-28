0f8ef77accad65d5e73e1fd8a9efdc6544ff8aaf
kaifengjin <jkfbupt@gmail.com>
2015-10-28 20:07:49 +0800

    fix

--- redis-benchmark.cpp
+++ redis-benchmark.cpp
@@ -763,7 +763,7 @@ int main(int argc, const char **argv) {
     config.numclients = 50;
     config.requests = 100000;
     config.liveclients = 0;
-    config.el = aeCreateEventLoop(100000);
+    config.el = aeCreateEventLoop(400000);
     //aeCreateTimeEvent(config.el,1,showThroughput,NULL,NULL);
     config.keepalive = 1;
     config.datasize = 3;
